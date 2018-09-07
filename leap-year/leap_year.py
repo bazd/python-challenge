04d822956d94a7f4b145d3a0f667c0a7d19e32ab
Barry Dawson <barry.dawson@ft.com>
2018-09-07 15:40:50 +0100

    Script now works

--- leap-year/leap_year.py
+++ leap-year/leap_year.py
@@ -3,30 +3,24 @@
    Python 3
 """
 
+import argparse
+parser = argparse.ArgumentParser()
+
 def leap_year_check(year):
     # Function to return True if leap year, and False if not    
-    
-    century_check = year / 100
-    if int(century_check):
-        print(year, 'is a century year')
-        leap_century_check = year / 400
-        if int(leap_century_check):
-            print(year, 'is a leap year')
-            return True
-        else:
-            print(year, 'is not a leap year')
-            return False
-    
-    leap_check = year / 4
-    if int(leap_check):
+
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
         print(year, 'is a leap year')
         return True
     else:
         print(year, 'is not a leap year')
-        return False     
+        return False  
 
+def main():
+    year = input("Enter a year to check if it's a leap year: ")
+    year = int(year) # Convert string input to integer
+    result = leap_year_check(year)
+    print(result)
 
-year = input("Enter a year to check if it's a leap year: ")
-year = int(year) # Convert string input to integer
-result = leap_year_check(year)
-print(result)
+if __name__ == "__main__":
+    main()
